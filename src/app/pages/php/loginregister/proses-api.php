fd14985d761340ce92b9f9b8c1a4faed3a30a481
Roby Wahyudi <robiwahyudi12@gmail.com>
2019-08-01 10:50:02 +0700

    Edit localhost
    mengubah alamat localhost

--- src/app/pages/php/loginregister/proses-api.php
+++ src/app/pages/php/loginregister/proses-api.php
@@ -12,7 +12,8 @@
   $today    = date('Y-m-d');
 
 
-
+// tabel user
+//-------------------------------------------------------------------------------------------------
    if($postjson['aksi']=="login"){
     $query = mysqli_query($mysqli, "SELECT * FROM user WHERE username='$postjson[username]' AND password='$postjson[password]'");
     $check = mysqli_num_rows($query);
@@ -20,6 +21,7 @@
     if($check>0){
       $data = mysqli_fetch_array($query);
       $datauser = array(
+	  'nik' =>$data['nik'],
         'username' => $data['username'],
         'password' => $data['password']
       );
@@ -31,7 +33,9 @@
 
     echo $result;
   }
-
+    //------------------------------------------------------------------------------------
+  // Tabel user // register
+//--------------------------------------------------------------------------------------
   elseif($postjson['aksi']=="register"){
     $query = mysqli_query($mysqli, "INSERT INTO user SET
       username = '$postjson[username]',
@@ -54,8 +58,28 @@
 
     echo $result;
   }
-  // Test
-  /*  elseif($postjson['aksi']=='getdata'){
+  //------------------------------------------------------------------------------------
+  // Tabel Kurir
+//--------------------------------------------------------------------------------------
+    elseif($postjson['aksi']=="registerkurir"){
+    $query = mysqli_query($mysqli, "INSERT INTO kurir SET
+      id_kurir = '$postjson[id_kurir]',
+      id_toko = '5',
+	  id_user = '$postjson[id_user]',
+	  nama = '$postjson[nama]',
+	  kendaraan = '$postjson[kendaraan]',
+	  nopol = '$postjson[nopol]'
+    ");
+
+    if($query) $result = json_encode(array('success'=>true));
+    else $result = json_encode(array('success'=>false, 'msg'=>'Coba Lagi'));
+
+    echo $result;
+  }
+   //------------------------------------------------------------------------------------
+  // Tabel Barang tampil barang
+//--------------------------------------------------------------------------------------
+  elseif($postjson['aksi']=='getdata'){
   	$data = array();
   	$query = mysqli_query($mysqli, "SELECT * FROM barang");
 
@@ -64,10 +88,10 @@
   		$data[] = array(
   			'id' => $row['id'],
   			'nama' => $row['nama'],
-			'id_toko' => $row['id_toko'],
-			'kategori' => $row['kategori'],
-  			'stok' => $row['stok'],
-  			'harga' => $row['harga'],
+  			'id_toko' => $row['id_toko'],
+  			'kategori' => $row['kategori'],
+			'stok' => $row['stok'],
+			'harga' => $row['harga'],
 			'kondisi' => $row['kondisi'],
 			'satuan' => $row['satuan'],
 			'gambar' => $row['gambar'],
@@ -81,6 +105,86 @@
   	echo $result;
 
   }
-*/
+      //------------------------------------------------------------------------------------
+  // Tabel barang // hapus
+//--------------------------------------------------------------------------------------
+    elseif($postjson['aksi']=='hapusbarang'){
+  	$query = mysqli_query($mysqli, "DELETE FROM barang WHERE id='$postjson[id]'");
+
+  	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
+  	else $result = json_encode(array('success'=>false, 'result'=>'error'));
+
+  	echo $result;
+
+  }
+    //------------------------------------------------------------------------------------
+  // Tabel barang // input
+//--------------------------------------------------------------------------------------
+  else if($postjson['aksi']=='inputbarang'){
+
+  	$query = mysqli_query($mysqli, "INSERT INTO barang SET
+  		nama = '$postjson[nama]',
+  		id_toko = 'T-001',
+		kategori = '$postjson[kategori]',
+		stok = '$postjson[stok]',
+		harga = '$postjson[harga]',
+		kondisi = '$postjson[kondisi]',
+		satuan = '$postjson[satuan]',
+		gambar = '$postjson[gambar]'
+		
+  		
+  	");
+
+  	$idcust = mysqli_insert_id($mysqli);
+
+  	if($query) $result = json_encode(array('success'=>true, 'customerid'=>$idcust));
+  	else $result = json_encode(array('success'=>false));
+
+  	echo $result;
+
+  }
+    //------------------------------------------------------------------------------------
+  // Tabel barang // Update 
+//--------------------------------------------------------------------------------------
+   elseif($postjson['aksi']=='updateproduk'){
+  	$query = mysqli_query($mysqli, "UPDATE barang SET 
+  		nama = '$postjson[nama]',
+  		id_toko = 'T-001',
+		kategori = '$postjson[kategori]',
+		stok = '$postjson[stok]',
+		harga = '$postjson[harga]',
+		kondisi = '$postjson[kondisi]',
+		satuan = '$postjson[satuan]',
+		gambar = '$postjson[gambar]' WHERE id='$postjson[id]'");
+
+  	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
+  	else $result = json_encode(array('success'=>false, 'result'=>'error'));
+
+  	echo $result;
+
+  }
+    //------------------------------------------------------------------------------------
+  // Tabel toko
+//--------------------------------------------------------------------------------------
+   elseif($postjson['aksi']=="registertoko"){
+    $query = mysqli_query($mysqli, "INSERT INTO toko SET
+     
+    id_toko = '$postjson[id_toko]',
+    id_user = '$postjson[id_user]',
+    nama_toko = '$postjson[nama_toko]',
+    alamat_toko = '$postjson[alamat_toko]',
+    kota = '$postjson[kota]',
+    kode_pos = '$postjson[kode_pos]',
+    kontak = '$postjson[kontak]',
+    keterangan = '$postjson[keterangan]',
+    logo = 'tos.jpg'
+    ");
+
+    if($query) $result = json_encode(array('success'=>true));
+    else $result = json_encode(array('success'=>false, 'msg'=>'error, please try again'));
+
+    echo $result;
+  }
 
 ?>
+	
